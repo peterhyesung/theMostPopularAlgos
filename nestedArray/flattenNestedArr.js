8d3a31d5a200c46cc916ebe6f0bdd36368d32bf6
Sumin Kim <ppsm920@gmail.com>
2021-05-07 11:09:30 -0700

    solved flatten a deeply nested array with reduce and concat only

--- nestedArray/flattenNestedArr.js
+++ nestedArray/flattenNestedArr.js
@@ -3,28 +3,30 @@
 
 function flatten(array) {
 // 1. use while with a recursive helper function 
-  //initialize variable and assign an empty array as a value;
-  const flattenArr = [];
-  //helper function: 
-  const helpFlattening = (array) => {
-    //declare a new variable called counter;
-    let counter = 0;
-    //iterate the passed in array 
-    while (counter < array.length){
-      const currEl = array[counter]
-      //check if the element is an array, 
-      //if so, return the helperFunc with currEl;
-      if (Array.isArray(currEl)) helpFlattening(currEl);
-      //otherwise push the currEl to flattenArr
-      else flattenArr.push(currEl)
-      //increment counter by 1;
-      counter++ 
-    }
-  }
-  //invoke the helper function;
-  helpFlattening(array);
-  //return flattenArr; 
-  return flattenArr;
-
+  // //initialize variable and assign an empty array as a value;
+  // const flattenArr = [];
+  // //helper function: 
+  // const helpFlattening = (array) => {
+  //   //declare a new variable called counter;
+  //   let counter = 0;
+  //   //iterate the passed in array 
+  //   while (counter < array.length){
+  //     const currEl = array[counter]
+  //     //check if the element is an array, 
+  //     //if so, return the helperFunc with currEl;
+  //     if (Array.isArray(currEl)) helpFlattening(currEl);
+  //     //otherwise push the currEl to flattenArr
+  //     else flattenArr.push(currEl)
+  //     //increment counter by 1;
+  //     counter++ 
+  //   }
+  // }
+  // //invoke the helper function;
+  // helpFlattening(array);
+  // //return flattenArr; 
+  // return flattenArr;
+//2. use reduce + concat 
+  return array.reduce((acc, val) => 
+    Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), []);
 }
 console.log(flatten([1, [2, 3, [4]]])); //[1, 2, 3, 4]
